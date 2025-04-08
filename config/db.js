32d9d3fa2d00d4ba3c28fd0e48bf2c92a89b844c
athalafk <athalafarastya@gmail.com>
2025-04-09 00:23:04 +0700

    update connection

--- config/db.js
+++ config/db.js
@@ -2,13 +2,15 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  host: process.env.AZURE_MYSQL_HOST,
+  user: process.env.AZURE_MYSQL_USER,
+  password: process.env.AZURE_MYSQL_PASSWORD,
+  database: process.env.AZURE_MYSQL_DATABASE,
+  port: process.env.AZURE_MYSQL_PORT || 3306,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
 });
 
 pool.getConnection()
